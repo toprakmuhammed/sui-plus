9f65a49327fe2073efeaa520e63633469be8331e
Muhammed Toprak <muhammedtoprak@live.com>
2025-10-25 18:18:19 +0300

    refactor navigation structure and styles; add responsive hamburger menu functionality

---
+++ script.js
@@ -0,0 +1,6 @@
+const menuToggle = document.getElementById("menuToggle");
+const navLinks = document.querySelector(".nav-links");
+
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
